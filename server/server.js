eaba7cc52bd19284c4c694e0266d12b47e0c1728
Sreekanth <avulasreekanth1234@gmail.com>
2026-06-22 15:25:37 +0530

    Fix redirect URI

--- server/server.js
+++ server/server.js
@@ -12,7 +12,6 @@ app.use(express.json());
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-
 const REDIRECT_URI =
   "https://salesforce-metadata-manager.vercel.app";
 
